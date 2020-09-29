4c0bfb1928a87248331d14348474afece9ef4851
RoninKagome <lesorubshayan@gmail.com>
2020-09-29 14:50:13 +0300

    added classes

---
+++ src/com/company/Main.java
@@ -0,0 +1,90 @@
+package com.company;
+
+class Schedule {
+
+}
+
+class Salary {
+
+}
+
+class Bonus {
+
+}
+
+class Enter {
+
+
+}
+class Exit {
+
+
+}
+class Worker {
+    private void assemble() {
+
+    }
+    private void repair() {
+
+    }
+    private void giveTester() {
+
+    }
+
+}
+class Computer {
+
+
+}
+class Component {
+
+
+}
+class Manager {
+    private void buy() {
+
+    }
+    private void sell() {
+
+    }
+    private void giveWorker() {
+
+    }
+
+}
+class Financier {
+    private void giveManager() {
+
+    }
+
+}
+class Money {
+
+
+}
+class Human {
+
+
+
+}
+
+class Tester {
+    private void check() {
+
+    }
+    private void giveWorker() {
+
+    }
+    private void giveManager() {
+
+    }
+}
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
